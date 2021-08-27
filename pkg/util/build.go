aa06cd14922c3ccb7e4aa33d25eed5d6131a5d9f
BlueWhaleKo <koko8624@gmail.com>
2021-08-28 02:07:30 +0900

    feature: change return type of BuildImage() and PushImage()

--- pkg/util/build.go
+++ pkg/util/build.go
@@ -11,15 +11,14 @@ import (
 	"github.com/docker/docker/client"
 )
 
-func BuildImage(c *client.Client, dockerfile io.Reader, opt types.ImageBuildOptions) error {
-
+func BuildImage(c *client.Client, dockerfile io.Reader, opt types.ImageBuildOptions) (types.ImageBuildResponse, error) {
 	buf := new(bytes.Buffer)
 	tw := tar.NewWriter(buf)
 	defer tw.Close()
 
 	readall, err := ioutil.ReadAll(dockerfile)
 	if err != nil {
-		return err
+		return types.ImageBuildResponse{}, err
 	}
 
 	err = tw.WriteHeader(&tar.Header{
@@ -27,21 +26,21 @@ func BuildImage(c *client.Client, dockerfile io.Reader, opt types.ImageBuildOpti
 		Size: int64(len(readall)),
 	})
 	if err != nil {
-		return err
+		return types.ImageBuildResponse{}, err
 	}
 
 	_, err = tw.Write(readall)
 	if err != nil {
-		return err
+		return types.ImageBuildResponse{}, err
 	}
 
 	tarReader := bytes.NewReader(buf.Bytes())
 	res, err := c.ImageBuild(context.Background(), tarReader, opt)
 	if err != nil {
-		return err
+		return types.ImageBuildResponse{}, err
 	}
 
 	Print(res.Body)
 
-	return nil
+	return res, nil
 }
--- pkg/util/push.go
+++ pkg/util/push.go
@@ -2,18 +2,12 @@ package util
 
 import (
 	"context"
+	"io"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
-func PushImage(c *client.Client, image string, opt types.ImagePushOptions) error {
-	reader, err := c.ImagePush(context.Background(), image, opt)
-	if err != nil {
-		return err
-	}
-
-	Print(reader)
-
-	return nil
+func PushImage(c *client.Client, image string, opt types.ImagePushOptions) (io.ReadCloser, error) {
+	return c.ImagePush(context.Background(), image, opt)
 }
